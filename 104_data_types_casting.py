ba32a9913ffa3ed9454e16a96193d63bb525756c
Rory Stokes <RStokes@spartaglobal.com>
2019-09-23 13:55:38 +0100

    covered casting

---
+++ 104_data_types_casting.py
@@ -0,0 +1,14 @@
+#Casting
+# Casting is when you change an object data type to a specific data type
+
+# string to integer
+my_var = '10'
+print(type(my_var))
+
+my_casted_var = int(my_var)
+
+# integer/floats to string
+my_int_var = 14
+my_casted_str = str(my_int_var)
+
+print('my_casted_str:', type(my_casted_str))
