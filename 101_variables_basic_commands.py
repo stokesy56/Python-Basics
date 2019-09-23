54f04eef5e9fe1647f3ed68823e7c3d9c13c6590
Rory Stokes <RStokes@spartaglobal.com>
2019-09-23 11:03:54 +0100

    finished variables basic and numericals moving onto boolean now

---
+++ 101_variables_basic_commands.py
@@ -0,0 +1,31 @@
+# variables in python
+    #variable is a box. You can give it a name and put stuff inside
+
+#giving box name and putting stuff inside
+box_variable = 'Books'
+
+#looking inside the box
+print(box_variable)
+
+# variabels are mutable - can be changed/reassigned
+
+#Reassigning variable
+box_variable = 'CDs'
+print(box_variable)
+
+#Reassigning with integer
+#variable can have any data type inside
+box_variable = 14
+print(box_variable)
+
+# important python functions
+# print(arg)
+print('hello') #printing string
+print(box_variable) # printing variable
+print('hello', box_variable) #overloading with two arguments
+
+# type(arg)
+# output the data type of an object
+print(type('hello'))
+print(type(14))
+print(type(14.0))
---
+++ 102_data_types.py
@@ -0,0 +1,40 @@
+# Numerical Data types
+# - Int, long, float, complex
+#  These are numerical data types which we can use numerical operators
+
+# Complex and long we don't use much
+# COmplex can have imaginary numbers
+# long - are integers of unlimited size
+
+my_int = 10
+print(my_int)
+print(type(my_int))
+
+# Operator - add, subtract, divide and multiply
+# exponential
+
+print(4 / 2)
+print(5 // 2)  # keeps it a s integer/ drops decimal
+print(3 * 2)
+print(3 ** 2)
+
+# Modulus % gives remainder from division of two variables
+print(10 % 3)  ##gives 1 there are three 3's in 10 and 1 leftover
+
+# Comparison operator --> outputs boolean value
+# == is comparison operator
+# < / > is greater/smaller than
+# <= is less than or equal to
+# >= is greater than or equal to
+# != not equal
+# is or is not
+
+my_variable1 = 10
+my_variable2 = 13
+
+print(my_variable1 == my_variable2)
+print(my_variable2 > my_variable1)
+print(my_variable2 is 12)
+print(my_variable1 is not 11)
+
+#Boolean values
